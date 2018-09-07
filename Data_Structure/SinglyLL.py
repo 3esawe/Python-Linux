935cb7a675df37e76b19165a9eadf5e808ed0fb4
omar <omaroobaniessa@gmail.com>
2018-09-08 01:31:40 +0300

    modified SinglyLL

--- Data_Structure/SinglyLL.py
+++ Data_Structure/SinglyLL.py
@@ -8,17 +8,28 @@ class SinglyLL:
         self.tail = None
         self._size = 0
     def insert_first(self,value):
+
         oldnode = self.head
         self.head = Node(value)
         self.head.next = oldnode
         self._size += 1
+        if self.tail is None:
+            self.tail = self.head
 
 
-    def __repr__(self):
-        return f"{self.head.next.value}"
+    def insert_last(self, value):
+            oldtail = self.head
+            while oldtail.next is not None:
+                oldtail = oldtail.next
+            node = Node(value)
+            oldtail.next = node
+            self._size += 1
 
 
     def insert_at_position(self,value,position):
+        if position >= self._size:
+            self.insert_last(value)
+            return
         if position == 0:
             insert_first(value)
             self._size += 1
@@ -32,9 +43,24 @@ class SinglyLL:
         node = Node(value)
         node.next = temp
         prev.next = node
-    def delete(self):
-        self.head = self.head.next
+        self._size += 1
+
+    def delete(self, value):
+        temp = self.head
+        if (temp is not None):
+            if temp.value == value:
+                self.head = temp.next
+                temp = None
+                return
+        while (temp is not None):
+            if temp.value == value:
+                break
+            prev = temp
+            temp = temp.next
+        prev.next = temp.next
+        temp = None
         self._size -= 1
+
     def printnodes(self):
         temp = self.head
         while temp is not None:
@@ -48,5 +74,6 @@ nodeC.insert_first(5)
 nodeC.insert_first(7)
 nodeC.insert_first(8)
 nodeC.insert_at_position("omar",4)
+nodeC.delete(5)
 nodeC.printnodes()
 print(nodeC.size)
