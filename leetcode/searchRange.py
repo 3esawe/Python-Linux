a09a62fd92242995c4b6a4a3742b3b10aa39d3a4
omar <omaroobaniessa@gmail.com>
2018-09-16 16:48:45 +0300

    added solution

--- leetcode/searchRange.py
+++ leetcode/searchRange.py
@@ -1,22 +1,6 @@
 class Sulotion:
     def searchRange(self, nums, target):
-        indicies = []
-        while len(indicies) < 2:
-            position = self.positions(nums, target)
-            if type(position) is list:
-                if -1 in position:
-                    return [-1, -1]
-                else:
-                    return [0, 0]
-            indicies.append(position)
-        indicies.sort()
-        return indicies
 
-
-    def positions(self, nums, target):
-        if len(nums) == 1 and target in nums:
-            return [0, 0]
-        range_list = []
         lo = 0
         hi = len(nums) - 1
         while lo <= hi:
@@ -27,8 +11,32 @@ class Sulotion:
             elif target < nums[mid]:
                 hi = mid - 1
             else :
-                lo = mid + 1
+                return [self.first(nums, 0, mid, target), self.last(nums, mid + 1, len(nums)- 1, target)]
         return [-1, -1]
+
+
+    def first(self, nums, lo, hi, target):
+        while lo <= hi:
+            mid = (hi + lo)// 2
+            if  target > nums[mid]:
+                lo = mid +1
+            elif target > nums[mid - 1]:
+                return mid
+            else:
+                hi = mid - 1
+        return lo
+
+
+    def last(self, nums, lo, hi, target):
+        while lo <= hi:
+            mid = (hi + lo) // 2
+            if target < nums[mid]:
+                hi = mid + 1
+            elif target < nums[mid + 1]:
+                return mid
+            else:
+                lo = mid - 1
+        return hi
 c = Sulotion()
-kis = c.searchRange([1],1)
+kis = c.searchRange([5,7,7,8,8,10],8)
 print(kis)
